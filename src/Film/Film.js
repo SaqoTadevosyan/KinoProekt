ac4bcb55472b05c9da6d28cae27c67ad72fcf8d6
SaqoTadevosyan <sargistadevosyan14@gmail.com>
2020-05-25 18:57:37 +0300

    fix some bug

--- src/Film/Film.js
+++ src/Film/Film.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { ThemeContextConsumer } from "../themeContext";
+import fire from "../fire/config";
 import {
   faStar,
   faHeart,
@@ -20,151 +20,180 @@ import {
   faGlobe,
   faPercent,
 } from "@fortawesome/free-solid-svg-icons";
-import firebase from "../fire/config"
-export default function Film(props) {
-  let id = props.match.params.id || 0;
- 
-let updateView=(id,views)=>{
-  
+import firebase from "../fire/config";
+class Film extends Component {
+  state = {
+    data: [],
+  };
 
-  firebase.database().ref("/"+id).update({"views":views +1})
-}
-  return (
-    <ThemeContextConsumer>
-      {(context) => (
-        <div className={style.container}>
-          <div className={style.firstBlock}>
-         
-            <img src={context.film[id].img } />
-            <div className={style.info}>
-              <div className={style.details}>
-                <div className={style.category}>
-                  {context.film[id].category.map((elem) => {
-                    return elem + "   ";
-                  })}{" "}
+  componentDidMount = () => {
+    let db = [];
+    fire
+      .database()
+      .ref("/0" )
+      .once("value")
+      .then((snapshot) => {
+        db = snapshot.val();
+      })
+      .then(() => {
+        this.setState({
+          data: db,})})
+        
+      
+  };
+
+  updateView = (id, views) => {
+    firebase
+      .database()
+      .ref("/" + id)
+      .update({ views: views + 1 });
+  };
+
+  render() {
+    return (
+      <div className={style.container}>
+        <div className={style.firstBlock}>
+          <img src={this.state.data.img} />
+          <div className={style.info}>
+            <div className={style.details}>
+              <div className={style.category}>
+                {this.state.data.category ?    this.state.data.category.map((elem) => {
+                  return elem + "   ";
+                }):null}
+              </div>
+              <div className={style.name}>{this.state.data.name}</div>
+              <div className={style.info2}>
+                <div className={style.infoblock}>
+                  <div className={style.infokey}>
+                    {" "}
+                    <i>
+                      <FontAwesomeIcon icon={faClock} />
+                    </i>
+                    Time:
+                  </div>
+
+                  <div className={style.infovalue}>
+                    {this.state.data.long}minutes
+                  </div>
                 </div>
-                <div className={style.name}>{context.film[id].name}</div>
-                <div className={style.info2}>
-                  <div className={style.infoblock}>
-                    <div className={style.infokey}>
-                      {" "}
-                      <i>
-                        <FontAwesomeIcon icon={faClock} />
-                      </i>
-                      Time:
-                    </div>
-
-                    <div className={style.infovalue}>
-                      {context.film[id].long}minutes
-                    </div>
+                <div className={style.infoblock}>
+                  <div className={style.infokey}>
+                    {" "}
+                    <i>
+                      <FontAwesomeIcon icon={faPlay} />
+                    </i>
+                    Year:
                   </div>
-                  <div className={style.infoblock}>
-                    <div className={style.infokey}>
-                      {" "}
-                      <i>
-                        <FontAwesomeIcon icon={faPlay} />
-                      </i>
-                      Year:
-                    </div>
 
-                    <div className={style.infovalue}>
-                      {context.film[id].year}
-                    </div>
+                  <div className={style.infovalue}>{this.state.data.year}</div>
+                </div>
+                <div className={style.infoblock}>
+                  <div className={style.infokey}>
+                    {" "}
+                    <i>
+                      <FontAwesomeIcon icon={faFlag} />
+                    </i>
+                    Country:
                   </div>
-                  <div className={style.infoblock}>
-                    <div className={style.infokey}>
-                      {" "}
-                      <i>
-                        <FontAwesomeIcon icon={faFlag} />
-                      </i>
-                      Country:
-                    </div>
 
-                    <div className={style.infovalue}>
-                      {context.film[id].country}
-                    </div>
+                  <div className={style.infovalue}>
+                    {this.state.data.country}
                   </div>
-                  <div className={style.infoblock}>
-                    <div className={style.infokey}>
-                      {" "}
-                      <i>
-                        <FontAwesomeIcon icon={faGlobe} />
-                      </i>
-                      Language:
-                    </div>
+                </div>
+                <div className={style.infoblock}>
+                  <div className={style.infokey}>
+                    {" "}
+                    <i>
+                      <FontAwesomeIcon icon={faGlobe} />
+                    </i>
+                    Language:
+                  </div>
 
-                    <div className={style.infovalue}>
-                      {context.film[id].language}
-                    </div>
-                    
+                  <div className={style.infovalue}>
+                    {this.state.data.language}
                   </div>
-                  <div className={style.infoblock}>
-                    <div className={style.infokey}>
-                      {" "}
-                      <i>
-                        <FontAwesomeIcon icon={faPercent} />
-                      </i>
-                      Rating:
-                    </div>
+                </div>
+                <div className={style.infoblock}>
+                  <div className={style.infokey}>
+                    {" "}
+                    <i>
+                      <FontAwesomeIcon icon={faPercent} />
+                    </i>
+                    Rating:
+                  </div>
 
-                    <div className={style.infovalue}>
-                      {context.film[id].rating.map((elem)=>{
-                        return(   <i>
-                           <FontAwesomeIcon icon={faStar} />
-                         </i>)
-                      })}
-                    </div>
+                  <div className={style.infovalue}>
+                    { this.state.data.rating ?   this.state.data.rating.map((elem) => {
+                      return (
+                        <i>
+                          <FontAwesomeIcon icon={faStar} />
+                        </i>
+                      );
+                    }):null}
                   </div>
-                  
                 </div>
               </div>
-              <div className={style.about}><p>{context.film[id].info}</p></div>
-                
+            </div>
+            <div className={style.about}>
+              <p>{this.state.data.info}</p>
             </div>
           </div>
-         <div className={style.secondBlock}>
-           <div className={style.player}>
-             <div className={style.title}>
-                     <i>
-                        <FontAwesomeIcon icon={faVideo} />
-                      </i>
-                      <h3>Watch Now</h3>
-<div>
-  <button>Player 1</button>
-  <button>Player 2</button>
-</div>
-<div onClick={updateView.bind(this,id,context.film[id].views)}>
-<iframe width="1140" height="690" src={context.film[id].src} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div>
-             </div>
-                      
-           </div>
-         </div>
-          <div className={style.thirdBlock}>
-            <div className={style.title}>
-                        <i>
-                        <FontAwesomeIcon icon={faUsers} />
-                      </i>
-                      <h3 style={{color:"black"}}>Actors</h3>
-                      </div>
-                      <div className={style.actorsList}>
-            {context.film[id].cast.map((elem,index)=>{
-return( <div className={style.actor}>
-  <div>
-  <img src={elem[2] || "http://localhost:3000/up8.jpg"}></img>
- 
-    </div>
-  
-    <h4>{elem[1]}</h4>
-<h5>{elem[0]}</h5>
-
-</div>)
-            })}
-          </div>
-          </div>
-          
         </div>
-      )}
-    </ThemeContextConsumer>
-  );
+        <div className={style.secondBlock}>
+          <div className={style.player}>
+            <div className={style.title}>
+              <i>
+                <FontAwesomeIcon icon={faVideo} />
+              </i>
+              <h3>Watch Now</h3>
+              <div>
+                <button>Player 1</button>
+                <button>Player 2</button>
+              </div>
+              <div
+                onClick={this.updateView.bind(
+                  this,
+                  this.props.match.params.id,
+                  this.state.data.views
+                )}
+              >
+                <iframe
+                  width="1140"
+                  height="690"
+                  src={this.state.data.src}
+                  frameborder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                ></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={style.thirdBlock}>
+          <div className={style.title}>
+            <i>
+              <FontAwesomeIcon icon={faUsers} />
+            </i>
+            <h3 style={{ color: "black" }}>Actors</h3>
+          </div>
+          <div className={style.actorsList}>
+            {this.state.data.cast ?   this.state.data.cast.map((elem, index) => {
+              return (
+                <div className={style.actor}>
+                  <div>
+                    <img src={elem[2] || "http://localhost:3000/up8.jpg"}></img>
+                  </div>
+
+                  <h4>{elem[1]}</h4>
+                  <h5>{elem[0]}</h5>
+                </div>
+              );
+            }):null}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default Film;
