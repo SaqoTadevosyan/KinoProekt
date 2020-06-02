022e019def87b6127331a0b5cc9704bdbd675784
SaqoTadevosyan <sargistadevosyan14@gmail.com>
2020-06-02 20:56:39 +0300

    Updates

---
+++ precache-manifest.44e231b75f2591e840772452c7a61ee2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8645e962f0a03827aa5e53cb7ec27927",
+    "url": "/KinoProekt/index.html"
+  },
+  {
+    "revision": "6115581a1c74cd221618",
+    "url": "/KinoProekt/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "a7c16519b2dc0440eb86",
+    "url": "/KinoProekt/static/css/main.e04cc65b.chunk.css"
+  },
+  {
+    "revision": "6115581a1c74cd221618",
+    "url": "/KinoProekt/static/js/2.37d46006.chunk.js"
+  },
+  {
+    "revision": "996e97a9c59b2ad74d9e36e982ab30fe",
+    "url": "/KinoProekt/static/js/2.37d46006.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7c16519b2dc0440eb86",
+    "url": "/KinoProekt/static/js/main.f5138161.chunk.js"
+  },
+  {
+    "revision": "51c56e158f615353168b",
+    "url": "/KinoProekt/static/js/runtime-main.c793dffc.js"
+  }
+]);
