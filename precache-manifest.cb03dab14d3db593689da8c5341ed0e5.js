e9ca6537cf10533787e7362ad14c5bbfd82177a9
SaqoTadevosyan <55787148+SaqoTadevosyan@users.noreply.github.com>
2020-06-13 18:29:50 +0400

    tesst
    setsfe

---
+++ precache-manifest.cb03dab14d3db593689da8c5341ed0e5.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = [
+  {
+    "revision": "42d95547eaabee9a92fa",
+    "url": "/test1/static/css/main.7c18399c.chunk.css"
+  },
+  {
+    "revision": "42d95547eaabee9a92fa",
+    "url": "/test1/static/js/main.42d95547.chunk.js"
+  },
+  {
+    "revision": "4a7c7c02c35b36e19345",
+    "url": "/test1/static/css/1.ed114d5b.chunk.css"
+  },
+  {
+    "revision": "4a7c7c02c35b36e19345",
+    "url": "/test1/static/js/1.4a7c7c02.chunk.js"
+  },
+  {
+    "revision": "618ce9163d15f78d214e",
+    "url": "/test1/static/js/runtime~main.618ce916.js"
+  },
+  {
+    "revision": "c243ed71447c5ace17963a4a142226ec",
+    "url": "/test1/static/media/Asset-16.c243ed71.svg"
+  },
+  {
+    "revision": "7a036cd240be111152c6f8da9dc9b27d",
+    "url": "/test1/static/media/Asset-18.7a036cd2.svg"
+  },
+  {
+    "revision": "d55045de1f171e897e8a90529a0771f8",
+    "url": "/test1/static/media/Asset-19.d55045de.svg"
+  },
+  {
+    "revision": "440a52bafb9dc49488228b2307f244db",
+    "url": "/test1/static/media/01.440a52ba.gif"
+  },
+  {
+    "revision": "f60bb68d159d735ac9141fbf263e1bab",
+    "url": "/test1/static/media/06.f60bb68d.gif"
+  },
+  {
+    "revision": "94de235ee1e6da95f09a36ba41f00fc2",
+    "url": "/test1/static/media/07.94de235e.gif"
+  },
+  {
+    "revision": "044a1b684e4cb60875efa7cab54f6c39",
+    "url": "/test1/static/media/03.044a1b68.gif"
+  },
+  {
+    "revision": "955f0e28139aae580355fb6c1440fca8",
+    "url": "/test1/static/media/Asset-17.955f0e28.svg"
+  },
+  {
+    "revision": "fe931631e0a4ce1657dd539cacaab708",
+    "url": "/test1/static/media/Asset-23.fe931631.svg"
+  },
+  {
+    "revision": "4405405dc6e43b08dc7e6ec761e8774e",
+    "url": "/test1/static/media/avatar-1.4405405d.png"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/test1/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "a720f17aa773e493a7ebf8b08459e66c",
+    "url": "/test1/static/media/Roboto-Italic.a720f17a.ttf"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/test1/static/media/Roboto-Black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/test1/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/test1/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "d9f4d75565a6023e7b53a888e1b9eafe",
+    "url": "/test1/static/media/searchBar_background.d9f4d755.png"
+  },
+  {
+    "revision": "d49e985815249aea761330fbd31e1e2e",
+    "url": "/test1/static/media/fontawesome-webfontd41d.d49e9858.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/test1/static/media/fontawesome-webfont3e6e.af7ae505.woff2"
+  },
+  {
+    "revision": "a4bc042e250c8a1c4ad127c9c3c50e9e",
+    "url": "/test1/static/media/fontawesome-webfont3e6e.a4bc042e.woff"
+  },
+  {
+    "revision": "d49e985815249aea761330fbd31e1e2e",
+    "url": "/test1/static/media/fontawesome-webfont3e6e.d49e9858.eot"
+  },
+  {
+    "revision": "187554eb3597e6ac3102b388b4038662",
+    "url": "/test1/static/media/materialdesignicons-webfontd41d.187554eb.eot"
+  },
+  {
+    "revision": "2f142ae946a9977d9c5992c64f2d4450",
+    "url": "/test1/static/media/fontawesome-webfont3e6e.2f142ae9.ttf"
+  },
+  {
+    "revision": "187554eb3597e6ac3102b388b4038662",
+    "url": "/test1/static/media/materialdesignicons-webfont772b.187554eb.eot"
+  },
+  {
+    "revision": "d1bdfb3838e2f78edf1ede85f56eabc9",
+    "url": "/test1/static/media/materialdesignicons-webfont772b.d1bdfb38.woff2"
+  },
+  {
+    "revision": "1f693ca7b305f5456abc2e7a2375d107",
+    "url": "/test1/static/media/fontawesome-webfont3e6e.1f693ca7.svg"
+  },
+  {
+    "revision": "a5a63c099747d5e3167dd87d1bef90ce",
+    "url": "/test1/static/media/materialdesignicons-webfont772b.a5a63c09.woff"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/test1/static/media/themify9f24.2c454669.eot"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/test1/static/media/themify9f24.a1ecc3b8.woff"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/test1/static/media/themify9f24.e23a7dca.ttf"
+  },
+  {
+    "revision": "af695aec1806605828e23a5afcf06f55",
+    "url": "/test1/static/media/materialdesignicons-webfont772b.af695aec.ttf"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/test1/static/media/themifyd41d.2c454669.eot"
+  },
+  {
+    "revision": "cba68f986e60df8c74f4a53c3e39595c",
+    "url": "/test1/static/media/themify9f24.cba68f98.svg"
+  },
+  {
+    "revision": "676da203e4a9299ba3e7d8aac56492f6",
+    "url": "/test1/static/media/bg.676da203.jpg"
+  },
+  {
+    "revision": "c4bdef0c7c656841de5635738ff2d793",
+    "url": "/test1/static/media/Asset-40.c4bdef0c.svg"
+  },
+  {
+    "revision": "acfab2230421fc9aaa2a4b47d0500ec1",
+    "url": "/test1/static/media/Asset-40.acfab223.svg"
+  },
+  {
+    "revision": "43a99010ca0e5af7145a39c73ffbbbf5",
+    "url": "/test1/static/media/Asset-28.43a99010.svg"
+  },
+  {
+    "revision": "2641d995110b16a8e0d05299b487ccb3",
+    "url": "/test1/static/media/Asset-28.2641d995.svg"
+  },
+  {
+    "revision": "191a53c883e987056e9e2b71b5764836",
+    "url": "/test1/static/media/materialdesignicons-webfont772b.191a53c8.svg"
+  },
+  {
+    "revision": "a12c573d000a0cea8c13597d2f76fe80",
+    "url": "/test1/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/KinoProekt/precache-manifest.23561cef86dd23f84bea64b056a1ff6b.js"
+  "/test1/precache-manifest.cb03dab14d3db593689da8c5341ed0e5.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/KinoProekt/index.html"), {
+workbox.routing.registerNavigationRoute("/test1/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
